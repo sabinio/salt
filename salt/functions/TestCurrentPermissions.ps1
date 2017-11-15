614a3740c16f1376d8ebccb8b34c18b3b8d5dd74
Unknown <richie@bzzzt.io>
2017-11-15 11:12:27 +0000

    check permissions
    if account running module is not sysadmin then we need to check that user has minimum permissions required to run powershell.
    
    Note - if runas account is a proxy then we do not currently have a check in place that account can login as proxy

--- salt/functions/TestCurrentPermissions.ps1
+++ salt/functions/TestCurrentPermissions.ps1
@@ -1,5 +1,5 @@
-Function Test-CurrentPermissions{
-        <#
+Function Test-CurrentPermissions {
+    <#
 .Synopsis
 Test permissions of current user to verify that htey have the correct permissions to execute deployment.
 .Description
@@ -16,41 +16,67 @@ Test-CurrentPermissions -SqlInstance $SqlSvr
         [ValidateNotNullorEmpty()]
         $SqlInstance
     )
+    $missingPermissions = @()
     Write-Host "Yea boy!" -ForegroundColor Green -BackgroundColor DarkMagenta
     $script = "SELECT IS_SRVROLEMEMBER('sysadmin') as 'AmISysAdmin';"
     $SQLSysAdminPermissions = $SqlInstance.ConnectionContext.ExecuteScalar($script)
-    if ($SQLSysAdminPermissions -eq 1)
-    {
+    if ($SQLSysAdminPermissions -eq 1) {
         Write-Verbose "user is sysadmin on instance. No further checks required!" -Verbose
         return
     }
-    elseif($SQLSysAdminPermissions -eq 0)
-    {
-        Write-Verbose "user is not a member of the sysadmin sesrver role. Checking minimal permissions"
-        # /*
-        # USE msdb
-        # CREATE USER [asdf] FOR LOGIN [asdf]
-        # GO 
-        # GRANT SELECT ON dbo.sysschedules  TO [asdf]
-        # GRANT SELECT ON dbo.sysjobschedules  TO [asdf]
-        # GRANT SELECT ON dbo.sysjobs  TO [asdf]
-        # exec sp_addrolemember  @rolename = 'SQLAgentOperatorRole',  
-        #      @membername = 'asdf'  
-        # use master
-        # GO
-        
-        # CREATE USER [asdf] FOR LOGIN [asdf]
-        # GO
-        # GRANT SELECT ON master.dbo.sysprocesses  TO [asdf]
-        
-        # GRANT VIEW SERVER STATE TO [asdf]
-        
-        
-        # */
+    elseif ($SQLSysAdminPermissions -eq 0) {
+        Write-Verbose "user is not a member of the sysadmin sesrver role. Checking minimal permissions" -Verbose
+        $db = $SqlInstance.Databases.Item("msdb")
+        $ds = $db.ExecuteWithResults("
+        SELECT 'sysschedules' as 'permissions' FROM fn_my_permissions('sysschedules', 'OBJECT') 
+        WHERE subentity_name = ''
+        AND permission_name = 'SELECT'
+        ")
+        if ($ds.Tables[0].Rows[0]."permissions" -ne 'sysschedules') {
+            $missingPermissions += 'GRANT SELECT ON msdb.dbo.sysschedules'
+        }
+        $db = $SqlInstance.Databases.Item("msdb")
+        $ds = $db.ExecuteWithResults("
+        SELECT 'sysjobschedules' as 'permissions' FROM fn_my_permissions('sysjobschedules', 'OBJECT') 
+        WHERE subentity_name = ''
+        AND permission_name = 'SELECT'
+        ")
+        if ($ds.Tables[0].Rows[0]."permissions" -ne 'sysjobschedules') {
+            $missingPermissions += 'GRANT SELECT ON msdb.dbo.sysjobschedules'
+        }
+        $db = $SqlInstance.Databases.Item("msdb")
+        $ds = $db.ExecuteWithResults("
+        SELECT 'sysjobs' as 'permissions' FROM fn_my_permissions('sysjobs', 'OBJECT') 
+        WHERE subentity_name = ''
+        AND permission_name = 'SELECT'
+        ")
+        if ($ds.Tables[0].Rows[0]."permissions" -ne 'sysjobs') {
+            $missingPermissions += 'GRANT SELECT ON msdb.dbo.sysjobs'
+        }
+        $db = $SqlInstance.Databases.Item("master")
+        $ds = $db.ExecuteWithResults("
+        SELECT 'sysprocesses' as 'permissions' FROM fn_my_permissions('sysprocesses', 'OBJECT') 
+        WHERE subentity_name = ''
+        AND permission_name = 'SELECT'
+        ")
+        if ($ds.Tables[0].Rows[0]."permissions" -ne 'sysprocesses') {
+            $missingPermissions += 'GRANT SELECT ON master.dbo.sysprocesses'
+        }
+        $db = $SqlInstance.Databases.Item("master")
+        $ds = $db.ExecuteWithResults("SELECT IS_ROLEMEMBER('SQLAgentOperatorRole') AS 'SQLAgentOperatorRole';")
+        if ($ds.Tables[0].Rows[0]."SQLAgentOperatorRole" -ne 1) {
+            $missingPermissions += 'sp_addrolemember @rolename = "SQLAgentOperatorRole"'
+        }
+        $db = $SqlInstance.Databases.Item("master")
+        $ds = $db.ExecuteWithResults("SELECT HAS_PERMS_BY_NAME(null, null, 'VIEW SERVER STATE') AS 'ViewServerState';")
+        if ($ds.Tables[0].Rows[0]."ViewServerState" -ne 1) {
+            $missingPermissions += 'GRANT VIEW SERVER STATE'
+        }
+        if ($missingPermissions.Count -gt 0) {
+            Throw ("The following permissions need to be granted to the account running the PowerShell on the instance being deployed to `n{0}" -f ($missingPermissions -join " `n"))
+        }
     }
-    else{
-        Write-Error "Unable to check permisions."
-        Throw
+    else {
+        Throw "Unable to check permisions."
     }
-
 }
