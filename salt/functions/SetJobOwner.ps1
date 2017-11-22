2798b541fc33a5929ce67b6cdbcd4e6ee059c078
Unknown <richie@bzzzt.io>
2017-11-22 12:52:34 +0000

    update owner to check if owner
    Update job owner to check if it is owner before attempting to make a change, and if it is not sysadmin then to fail

--- salt/functions/SetJobOwner.ps1
+++ salt/functions/SetJobOwner.ps1
@@ -35,25 +35,34 @@ Set-JobOwner -JobToAlter $JobToAlter -root $root -SmoObjectConnection $SmoObject
         Write-Verbose 'Parameter jobOwner not included in XML document. Setting account running this task as owner of job. This requires sysadmin permissions.' -Verbose
         $jobOwner = $whoAmI 
     }
-    $db = $SmoObjectConnection.Databases.Item("msdb")
-    $ds = $db.ExecuteWithResults("SELECT IS_SRVROLEMEMBER('sysadmin') as 'AmISysAdmin';")
-    $AmISysAdmin = $ds.Tables[0].Rows[0]."AmISysAdmin"
-    if ($AmISysAdmin -eq 1) {
-        Write-Verbose "$whoAmI is sysadmin on instance, so job owner can be altered." -Verbose
-        try {
-            $JobToAlter.OwnerLoginName = $jobOwner
-            Write-Verbose "Updating Job Owner to $jobOwner..." -Verbose
-            $JobToAlter.Alter()
-            $JobToAlter.Refresh()
-        }
-        catch {
-            throw $_.Exception
-        }
-    }
-    elseif ($AmISysAdmin -eq 0) {
-        Write-Verbose "$whoAmI is NOT a sysadmin on the instance, job owner CANNOT be altered." -Verbose
+    Write-Verbose "Job Owner in XML is $jobOwner" -Verbose
+    Write-Verbose "Job Owner on Server is $($JobToAlter.OwnerLoginName)" -Verbose
+    if ($jobOwner -eq $JobToAlter.OwnerLoginName) {
+        Write-Verbose "Job Owner in XML matches Server. Not attempting to alter." -Verbose
+        Return
     }
     else {
-        Write-Warning "Unable to check whether or not user is sysadmin on the instance."
+        $db = $SmoObjectConnection.Databases.Item("msdb")
+        $ds = $db.ExecuteWithResults("SELECT IS_SRVROLEMEMBER('sysadmin') as 'AmISysAdmin';")
+        $AmISysAdmin = $ds.Tables[0].Rows[0]."AmISysAdmin"
+        if ($AmISysAdmin -eq 1) {
+            Write-Verbose "$whoAmI is sysadmin on instance, so job owner can be altered." -Verbose
+            try {
+                Write-Verbose "owner of Agent Job is$($JobToAlter.OwnerLoginName)"
+                $JobToAlter.OwnerLoginName = $jobOwner
+                Write-Verbose "Updating Job Owner to $jobOwner..." -Verbose
+                $JobToAlter.Alter()
+                $JobToAlter.Refresh()
+            }
+            catch {
+                throw $_.Exception
+            }
+        }
+        elseif ($AmISysAdmin -eq 0) {
+            Throw "$whoAmI is NOT a sysadmin on the instance, so job owner CANNOT be altered. Either set account running PowerShell to be sysadmin or alter Job Owner outside of this PowerShell Module."
+        }
+        else {
+            Write-Warning "Unable to check whether or not user is sysadmin on the instance."
+        }
     }
 }
